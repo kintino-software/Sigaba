ed1e7daa9b771d7e2d1225993a444e9a3d617a49
Marcelo Augusto <marceloaugustoxp@gmail.com>
2026-07-19 06:16:41 +0100

    .

--- Kintino.CipherConf.Crypto/Services/Algos/RSACipher.cs
+++ Kintino.CipherConf.Crypto/Services/Algos/RSACipher.cs
@@ -4,11 +4,11 @@ namespace Kintino.CipherConf.Crypto.Services.Algos;
 
 internal static class RSACipher
 {
-    private const int RsaKeySize = 3072; // Key size in bits. 3072 is the recommended size at july-2026
+    private const int RsaKeySizeInBits = 3072; // 3072 bits is the recommended size at july-2026
 
     public static void CreateNewKeyPair(out byte[] publicKey, out byte[] privateKey)
     {
-        using var rsa = RSA.Create(RsaKeySize);
+        using var rsa = RSA.Create(RsaKeySizeInBits);
 
         var publicKeyBytes = rsa.ExportRSAPublicKey();
         var privateKeyBytes = rsa.ExportRSAPrivateKey();
