2943c652a7ca9a4d3773ad398435bdb26d0fa952
Marcelo Augusto <marceloaugustoxp@gmail.com>
2026-08-08 03:50:16 +0100

    .

--- Sigaba.Cli.IntegrationTest/Cases/EncryptionTest.cs
+++ Sigaba.Cli.IntegrationTest/Cases/EncryptionTest.cs
@@ -14,21 +14,24 @@ public class EncryptionTest : BaseTest
     [Fact]
     public async Task Should_encrypt_all_files_in_directory_tree()
     {
+        // adding file in the root
         var file1Path = $"{cwd}/fileA.secrets.json".AsPath();
-        var file2Path = $"{cwd}/subdir/fileB.secrets.json".AsPath();
         var content1 = """
             {
                 "field1": "value 1",
                 "field2_secret": "secret value 2",
             }
             """;
+        Fs.AddFile(file1Path, new MockFileData(content1));
+
+        // adding a file in a subdirectory
+        var file2Path = $"{cwd}/subdir1/subdir2/fileB.secrets.json".AsPath();
         var content2 = """
             {
                 "field3": "value 3",
                 "field4_secret": "secret value 4",
             }
             """;
-        Fs.AddFile(file1Path, new MockFileData(content1));
         Fs.AddFile(file2Path, new MockFileData(content2));
 
         var app = CreateApp();
@@ -49,9 +52,9 @@ public class EncryptionTest : BaseTest
     }
 
     [Fact]
-    public async Task Should_not_encrypt_without_public_key()
+    public async Task Should_not_encrypt_without_valid_public_key()
     {
-        var file1Path = Fs.Path.Combine(cwd, "file_secrets.json");
+        var file1Path = $"{cwd}/file_secrets.json".AsPath();
         var content1 = """
             {
                 "field_secret": "secret value",
