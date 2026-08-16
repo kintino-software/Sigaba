5f0d279f8a1e04d3f1a55c2113784b21b81fa5b0
Marcelo Augusto <marceloaugustoxp@gmail.com>
2026-08-16 18:17:43 +0100

    .

--- Sigaba.Common.Test/Primitives/Crypto/Base/ByteLikeTests.cs
+++ Sigaba.Common.Test/Primitives/Crypto/Base/ByteLikeTests.cs
@@ -17,9 +17,9 @@ public class ByteLikeTests
     [Fact]
     public void Should_not_create_url_and_file_name_unsafe_chars_when_converting_to_base64()
     {
-        var obj = new DummyByteLike([251, 255, 254]);
+        var obj = new DummyByteLike([0xFE, 0xFF, 0xFE]);
         var actual = obj.ToBase64();
-        actual.Should().Be("-__-");
+        actual.Should().Be("_v_-");
     }
 
     // FromBase64
@@ -37,7 +37,7 @@ public class ByteLikeTests
     [Fact]
     public void Should_roundtrip_to_and_from_base64_string()
     {
-        var original = new DummyByteLike([0, 1, 3, 4, 5, 251, 255, 254]);
+        var original = new DummyByteLike([0x00, 0x01, 0x03, 0x04, 0x05, 0xFB, 0xFF, 0xFE]);
         var originalBase64 = original.ToBase64();
 
         var actual = DummyByteLike.FromBase64(originalBase64);
