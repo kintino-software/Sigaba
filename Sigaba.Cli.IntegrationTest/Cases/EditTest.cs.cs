88bae6a79aba032235b51e1489f89987a47c19a8
Marcelo Augusto <marceloaugustoxp@gmail.com>
2026-08-21 12:40:23 +0100

    cli: edit test

---
+++ Sigaba.Cli.IntegrationTest/Cases/EditTest.cs.cs
@@ -0,0 +1,38 @@
+﻿namespace Sigaba.Cli.Cases;
+
+public class EditTest : BaseTest
+{
+    private readonly string cwd;
+
+    public EditTest()
+    {
+        InitializeAppAsync().GetAwaiter().GetResult().Deconstruct(out _, out cwd);
+    }
+
+    private async Task Encrypt()
+    {
+        var app = this.CreateCommandApp();
+        await app.RunAsync(["encrypt"]);
+    }
+
+    [Theory]
+    [InlineData("file1.secrets.json")]
+    [InlineData("subdir1|file1.secrets.json")]
+    public async Task Should_edit_and_encrypt_files(string filePath)
+    {
+        var file1Path = Fs.Path.Combine(filePath.Split('|'));
+        var fileJson = """
+            {
+                "data_secret": 1234,
+                "data_public": "public"
+            }
+            """;
+        Fs.AddFile(file1Path, fileJson);
+        await Encrypt();
+
+        var result = await App.RunAsync(["edit", file1Path]);
+        TestContext.Current.TestOutputHelper.WriteLine(result.Output);
+
+        result.ExitCode.Should().Be(0);
+    }
+}
